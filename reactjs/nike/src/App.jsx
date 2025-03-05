43128cbe7fc37465aea84c1fefe8cb44656dbe0b
hpaliwal8 <hpaliwal@enphaseenergy.com>
2025-03-06 00:57:31 +0530

    startup code

---
+++ reactjs/nike/src/App.jsx
@@ -0,0 +1,32 @@
+const App = () => (
+  <main className="relative">
+      Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding"> 
+      PopularProducts
+    </section>
+    <section  className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      Special Offer
+    </section>
+    <section className="bg-pale-blue padding">
+      Customer Reviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footers
+    </section>
+    
+  </main>
+)
+
+export default App;
