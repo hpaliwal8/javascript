f0d42bde4ba5a878ebf2a4c4e05f34bc686a09bf
hpaliwal8 <hpaliwal@enphaseenergy.com>
2025-03-07 03:32:39 +0530

    Hero page

--- reactjs/nike/src/App.jsx
+++ reactjs/nike/src/App.jsx
@@ -1,29 +1,32 @@
+import Nav from "./components/Nav";
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+
 const App = () => (
   <main className="relative">
-      Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Nav />
+    <section className="xl:padding-ll wide:padding-rr padding-b">
+      <Hero />
     </section>
     <section className="padding"> 
-      PopularProducts
+      <PopularProducts />
     </section>
     <section  className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      Special Offer
+      <SpecialOffer />
     </section>
     <section className="bg-pale-blue padding">
-      Customer Reviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footers
+      <Footer />
     </section>
     
   </main>
--- reactjs/nike/tailwind.config.js
+++ reactjs/nike/tailwind.config.js
@@ -1,44 +1,62 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      fontSize: {
-        xs: ['12px', '16px'],
-        sm: ['14px', '20px'],
-        base: ['16px', '19.5px'],
-        lg: ['18px', '21.94px'],
-        xl: ['20px', '24.38px'],
-        '2xl': ['24px', '29.26px'],
-        '3xl': ['28px', '50px'],
-        '4xl': ['48px', '58px'],
-        '8xl': ['96px', '106px']
-      },
-      extend: {
-        fontFamily: {
-          palanquin: ['Palanquin', 'sans-serif'],
-          montserrat: ['Montserrat', 'sans-serif'],
+        fontSize: {
+            xs: ['12px', {
+                lineHeight: '16px',
+            }],
+            sm: ['14px', {
+                lineHeight: '20px',
+            }],
+            base: ['16px', {
+                lineHeight: '19.5px',
+            }],
+            lg: ['18px', {
+                lineHeight: '21.94px',
+            }],
+            xl: ['20px', {
+                lineHeight: '24.38px',
+            }],
+            '2xl': ['24px', {
+                lineHeight: '29.26px',
+            }],
+            '3xl': ['28px', {
+                lineHeight: '50px',
+            }],
+            '4xl': ['48px', {
+                lineHeight: '58px',
+            }],
+            '8xl': ['96px', {
+                lineHeight: '106px',
+            }]
         },
-        colors: {
-          'primary': "#ECEEFF",
-          "coral-red": "#FF6452",
-          "slate-gray": "#6D6D6D",
-          "pale-blue": "#F5F6FF",
-          "white-400": "rgba(255, 255, 255, 0.80)"
+        extend: {
+            fontFamily: {
+                palanquin: ['Palanquin', 'sans-serif'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+            colors: {
+                'primary': "#ECEEFF",
+                "coral-red": "#FF6452",
+                "slate-gray": "#6D6D6D",
+                "pale-blue": "#F5F6FF",
+                "white-400": "rgba(255, 255, 255, 0.80)"
+            },
+            boxShadow: {
+                '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+            },
+            backgroundImage: {
+                'hero': "url('assets/images/collection-background.svg')",
+                'card': "url('assets/images/thumbnail-background.svg')",
+            },
+            screens: {
+                "wide": "1440px"
+            }
         },
-        boxShadow: {
-          '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
-        },
-        backgroundImage: {
-          'hero': "url('assets/images/collection-background.svg')",
-          'card': "url('assets/images/thumbnail-background.svg')",
-        },
-        screens: {
-          "wide": "1440px"
-        }
-      },
     },
     plugins: [],
-  }
+}
