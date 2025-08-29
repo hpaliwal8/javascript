0a8c307dba33ac4215251f9d410af109f02aca93
hpaliwal8 <hpaliwal@enphaseenergy.com>
2025-08-28 22:13:28 -0400

    Fixing bug of navigating to other trips from within trips

--- projects/travel-agency/components/TripCard.tsx
+++ projects/travel-agency/components/TripCard.tsx
@@ -7,7 +7,7 @@ import { ChipDirective, ChipsDirective, ChipListComponent } from '@syncfusion/ej
 const TripCard = ({ id, name, location, imageUrl, tags, price }: TripCardProps) => {
     const path = useLocation();
     return (
-        <Link to={path.pathname === '/' || path.pathname.startsWith('/travel') ? `travel/${id}` : `/trips/${id}`} className="trip-card">
+        <Link to={path.pathname === '/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`} className="trip-card">
             <img src={imageUrl} alt={name} />
 
             <article>
